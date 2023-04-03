bb77bed83b0a3bfd756e8823f6134444a679c9dd
maltoze <maltoze@qq.com>
2023-04-03 10:55:36 +0800

    fix: make exit animation works

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -4,7 +4,7 @@ import { CgSpinnerAlt } from 'react-icons/cg';
 import { useRef } from 'preact/hooks';
 import useOnClickOutside from '../hooks/use-onclickoutside';
 import styles from '../styles/global.css';
-import { motion, Variants } from 'framer-motion';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
 
 type Props = {
   open: Signal<boolean>;
@@ -14,8 +14,8 @@ type Props = {
 };
 
 const variants: Variants = {
-  open: { opacity: 1, x: 0, height: '100%', width: '100%' },
-  closed: { opacity: 0, height: 0, width: 0 },
+  open: { opacity: 1, height: '100%', width: '100%' },
+  closed: { opacity: 0, height: '75%', width: '75%', transition: { duration: 0.1 } },
 };
 
 export default function App({ open, url, title, loading }: Props) {
@@ -31,60 +31,60 @@ export default function App({ open, url, title, loading }: Props) {
     loading.value = false;
   }
 
-  if (!open.value || !url.value) {
-    return null;
-  }
-
   return (
     <>
       <style type="text/css">{styles.toString()}</style>
-      <div className="fixed inset-0 overflow-y-auto">
-        <div className="flex h-[calc(100%-96px)] flex-col items-center justify-center py-12 px-20 md:px-32 lg:px-48">
-          <motion.div
-            className="shadow-xl"
-            ref={containerRef}
-            layout
-            animate="open"
-            exit="closed"
-            variants={variants}
-            initial="closed"
-          >
-            <div className="grid h-10 grid-cols-6 items-center gap-4 rounded-t-xl bg-zinc-200 px-4 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-100">
-              <div className="col-span-4 col-start-2 inline-flex items-center justify-center gap-2">
-                {loading.value ? (
-                  <i className="h-5 w-5 shrink-0 animate-spin">
-                    <CgSpinnerAlt size={22} />
-                  </i>
-                ) : (
-                  <i className="h-5 w-5"></i>
-                )}
-                <span className="truncate text-sm">{title}</span>
-              </div>
-              <div className="flex items-center justify-end ">
-                <a
-                  href={url.value}
-                  target="_blank"
-                  className="text-zinc-800 visited:text-zinc-800 hover:text-zinc-400 dark:text-zinc-100 dark:visited:text-zinc-100 [&:link]:text-zinc-800 [&:link]:hover:text-zinc-400 [&:link]:dark:text-zinc-100"
-                  rel="noreferrer"
-                >
-                  <HiArrowTopRightOnSquare size={20} />
-                </a>
-                <button
-                  className="m-0 border-0 bg-transparent p-0 pl-2 text-zinc-800 hover:text-zinc-400 dark:text-zinc-100"
-                  onClick={handleClose}
-                >
-                  <HiXMark size={24} />
-                </button>
-              </div>
+      <AnimatePresence>
+        {open.value && url.value && (
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex h-full flex-col items-center justify-center py-12 px-20 md:px-32 lg:px-48">
+              <motion.div
+                className="shadow-xl"
+                ref={containerRef}
+                layout
+                animate="open"
+                exit="closed"
+                variants={variants}
+                initial="closed"
+              >
+                <div className="grid h-10 grid-cols-6 items-center gap-4 rounded-t-xl bg-zinc-200 px-4 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-100">
+                  <div className="col-span-4 col-start-2 inline-flex items-center justify-center gap-2">
+                    {loading.value ? (
+                      <i className="h-5 w-5 shrink-0 animate-spin">
+                        <CgSpinnerAlt size={22} />
+                      </i>
+                    ) : (
+                      <i className="h-5 w-5"></i>
+                    )}
+                    <span className="truncate text-sm">{title}</span>
+                  </div>
+                  <div className="flex items-center justify-end ">
+                    <a
+                      href={url.value}
+                      target="_blank"
+                      className="text-zinc-800 hover:text-zinc-400 dark:text-zinc-100"
+                      rel="noreferrer"
+                    >
+                      <HiArrowTopRightOnSquare size={20} />
+                    </a>
+                    <button
+                      className="m-0 border-0 bg-transparent p-0 pl-2 text-zinc-800 hover:text-zinc-400 dark:text-zinc-100"
+                      onClick={handleClose}
+                    >
+                      <HiXMark size={24} />
+                    </button>
+                  </div>
+                </div>
+                <iframe
+                  src={url.value}
+                  onLoad={handleOnLoad}
+                  className="h-[calc(100%-40px)] w-full border-none bg-zinc-100 dark:bg-zinc-600"
+                />
+              </motion.div>
             </div>
-            <iframe
-              src={url.value}
-              onLoad={handleOnLoad}
-              className="h-[calc(100%-40px)] w-full border-none bg-zinc-100 dark:bg-zinc-600"
-            />
-          </motion.div>
-        </div>
-      </div>
+          </div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
