f6a31977f2e2f9cee55f94ec0b5193c4e38fbfd4
maltoze <maltoze@qq.com>
2023-04-07 15:35:19 +0800

    WIP

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -2,15 +2,14 @@ import { Signal } from '@preact/signals';
 import { RxCross1, RxOpenInNewWindow } from 'react-icons/rx';
 import { CgSpinnerAlt } from 'react-icons/cg';
 import { useRef } from 'preact/hooks';
-import useOnClickOutside from '../hooks/use-onclickoutside';
 import styles from '../styles/global.css';
 import { AnimatePresence, motion, Variants } from 'framer-motion';
 
 type Props = {
-  open: Signal<boolean>;
   url: Signal<string | null>;
   title: Signal<string | null>;
   loading: Signal<boolean>;
+  onClose: () => void;
 };
 
 const variants: Variants = {
@@ -23,15 +22,9 @@ const variants: Variants = {
   },
 };
 
-export default function App({ open, url, title, loading }: Props) {
+export default function App({ url, title, loading, onClose }: Props) {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
-
-  function handleClose() {
-    open.value = false;
-  }
-
-  useOnClickOutside(containerRef, handleClose);
 
   function handleOnLoad() {
     loading.value = false;
@@ -67,55 +60,51 @@ export default function App({ open, url, title, loading }: Props) {
     <>
       <style type="text/css">{styles.toString()}</style>
       <AnimatePresence>
-        {open.value && url.value && (
-          <div className="fixed inset-0 z-[2147483647] overflow-y-auto">
-            <div className="flex h-full flex-col items-center justify-center py-12 px-20 md:px-32 lg:px-48">
-              <motion.div
-                className="shadow-xl"
-                ref={containerRef}
-                animate="open"
-                exit="closed"
-                variants={variants}
-                initial="closed"
-              >
-                <div className="grid h-10 grid-cols-6 items-center gap-4 rounded-t-xl bg-zinc-200 px-4 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-100">
-                  <div className="col-span-4 col-start-2 inline-flex items-center justify-center gap-2">
-                    {loading.value ? (
-                      <i className="h-5 w-5 shrink-0 animate-spin">
-                        <CgSpinnerAlt size={22} />
-                      </i>
-                    ) : (
-                      <i className="h-5 w-5"></i>
-                    )}
-                    <span className="truncate text-sm">{title}</span>
-                  </div>
-                  <div className="flex items-center justify-end gap-2">
-                    <button
-                      onClick={handleOpenInNewTab}
-                      className="header-icon-btn"
-                    >
-                      <RxOpenInNewWindow size={20} />
-                    </button>
-                    <button
-                      className="header-icon-btn rotate-[270deg]"
-                      onClick={handleOpenInMainFrame}
-                    >
-                      <RxOpenInNewWindow size={20} />
-                    </button>
-                    <button className="header-icon-btn" onClick={handleClose}>
-                      <RxCross1 size={18} />
-                    </button>
-                  </div>
-                </div>
-                <iframe
-                  ref={iframeRef}
-                  src={url.value}
-                  onLoad={handleOnLoad}
-                  className="h-[calc(100%-40px)] w-full border-none bg-white"
-                />
-              </motion.div>
+        {url.value && (
+          <motion.div
+            className="h-full w-full shadow-xl"
+            ref={containerRef}
+            animate="open"
+            exit="closed"
+            variants={variants}
+            initial="closed"
+          >
+            <div className="grid h-10 grid-cols-6 items-center gap-4 rounded-t-xl bg-zinc-200 px-4 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-100">
+              <div className="col-span-4 col-start-2 inline-flex items-center justify-center gap-2">
+                {loading.value ? (
+                  <i className="h-5 w-5 shrink-0 animate-spin">
+                    <CgSpinnerAlt size={22} />
+                  </i>
+                ) : (
+                  <i className="h-5 w-5"></i>
+                )}
+                <span className="truncate text-sm">{title}</span>
+              </div>
+              <div className="flex items-center justify-end gap-2">
+                <button
+                  onClick={handleOpenInNewTab}
+                  className="header-icon-btn"
+                >
+                  <RxOpenInNewWindow size={20} />
+                </button>
+                <button
+                  className="header-icon-btn rotate-[270deg]"
+                  onClick={handleOpenInMainFrame}
+                >
+                  <RxOpenInNewWindow size={20} />
+                </button>
+                <button className="header-icon-btn" onClick={onClose}>
+                  <RxCross1 size={18} />
+                </button>
+              </div>
             </div>
-          </div>
+            <iframe
+              ref={iframeRef}
+              src={url.value}
+              onLoad={handleOnLoad}
+              className="h-[calc(100%-40px)] w-full border-none bg-white"
+            />
+          </motion.div>
         )}
       </AnimatePresence>
     </>
--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,10 +1,12 @@
 import { signal } from '@preact/signals';
 import { render } from 'preact';
 import App from './App';
+import { computePosition, shift, flip, offset } from '@floating-ui/dom';
+import getEventTarget from '../common/utils';
 
+const rootId = '__maltoze-linkpopper';
 const containerId = '__maltoze_linkpopper-container';
 
-const open = signal(false);
 const url = signal<string | null>(null);
 const title = signal<string | null>(null);
 const loading = signal(false);
@@ -28,10 +30,13 @@ function shouldHandleClickEvent(href: string) {
   return true;
 }
 
-// function destroyApp() {
-//   const container = document.querySelector(`#${containerId}`);
-//   container && render(null, container);
-// }
+function handleOnClose() {
+  const rootNode: HTMLElement | null = document.querySelector(`#${rootId}`);
+  rootNode && (rootNode.style.display = 'none');
+  // const container = document.querySelector(`#${containerId}`);
+  // container && render(null, container);
+  url.value = null;
+}
 
 function handleWindowClickEvent(event: MouseEvent) {
   const composedPath = event.composedPath();
@@ -43,31 +48,73 @@ function handleWindowClickEvent(event: MouseEvent) {
       event.preventDefault();
       event.stopPropagation();
 
-      open.value = true;
       url.value = target.href;
       title.value = target.text;
       loading.value = true;
+
+      const rootNode: HTMLElement | null = document.querySelector(`#${rootId}`);
+      if (rootNode) {
+        if (rootNode.style.display === 'none') {
+          rootNode.style.display = 'block';
+        }
+        computePosition(target, rootNode, {
+          placement: 'right',
+          middleware: [offset(10), flip(), shift()],
+        }).then(({ placement }) => {
+          if (placement === 'left') {
+            Object.assign(rootNode.style, {
+              left: `0px`,
+              top: '0px',
+              right: null,
+            });
+          } else {
+            Object.assign(rootNode.style, {
+              right: '0px',
+              top: '0px',
+              left: null,
+            });
+          }
+        });
+      }
     }
   }
 }
 
 async function main() {
   const root = document.createElement('div');
-  root.id = '__maltoze-linkpopper';
+  root.id = rootId;
+  root.style.position = 'fixed';
+  root.style.zIndex = '2147483647';
+  root.style.height = '100vh';
+  root.style.width = '50%';
+  root.style.display = 'none';
   document.body.appendChild(root);
 
   root.attachShadow({ mode: 'open' });
 
   const appContainer = document.createElement('div');
   appContainer.id = containerId;
+  appContainer.style.width = '100%';
+  appContainer.style.height = '100%';
   root.shadowRoot?.appendChild(appContainer);
 
   render(
-    <App open={open} url={url} title={title} loading={loading} />,
+    <App url={url} title={title} loading={loading} onClose={handleOnClose} />,
     appContainer
   );
 
   window.addEventListener('click', handleWindowClickEvent, { capture: true });
+
+  const mouseDownListener = (event: MouseEvent | TouchEvent) => {
+    if (
+      appContainer.contains(getEventTarget(event) as Node) ||
+      event.composedPath().find((node) => node instanceof HTMLAnchorElement)
+    ) {
+      return;
+    }
+    handleOnClose();
+  };
+  document.addEventListener('mousedown', mouseDownListener);
 }
 
 main();
