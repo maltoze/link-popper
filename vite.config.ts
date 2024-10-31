0ad739ca9674e70b4cbf57e8b112344a166a4ede
maltoze <atonezzz@gmail.com>
2024-10-31 09:30:42 +0800

    refactor: disable dynamic URL web accessible resources in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     preact(),
     webExtension({
       manifest: isFirefox ? manifestV2 : manifest,
+      useDynamicUrlWebAccessibleResources: false,
     }),
   ],
   build: {
